5bec092115f5f9dd69c975908c9e4ccccfd21cc6
tmtic <tmtic@hotmail.com>
2023-12-16 11:47:07 -0300

    segundo commit

--- msg.php
+++ msg.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>DEVSEC - MSG</title>
+    <title>DEVSEC - MSG2</title>
   </head>
   <body>
 
@@ -26,3 +26,4 @@ $output=shell_exec("curl http://localhost/".$_POST['msg']);
 echo $output;
 }
 ?>
+
